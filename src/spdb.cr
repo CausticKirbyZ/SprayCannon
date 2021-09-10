ca8dbe6e18e94b32619782330409026fe1775b39
CausticKirbyZ <mikehedlund20@gmail.com>
2021-09-09 23:25:58 -0500

    added a export feature to spdb to export the tables to csv

--- src/spdb.cr
+++ src/spdb.cr
@@ -104,8 +104,104 @@ while x = gets
 
 
 
+
+
+
+
+
+
+
+        ######################### export features ############################
+    when "export sprayed"
+        puts "Exporting SPRAYED to exported_spdb_sprayed.csv ... "
+        File.delete("./exported_spdb_sprayed.csv") if File.exists? "./exported_spdb_sprayed.csv"
+        file = File.new("./exported_spdb_sprayed.csv", "w")
+        file.puts "Username,Password,Time"
+        begin 
+            db.query "select username.username, password.password, passwords_sprayed.date_time
+            from username, password, passwords_sprayed
+            where 
+            username.usernameid = passwords_sprayed.usernameid
+            and 
+            password.passwordid = passwords_sprayed.passwordid;" do |rs|
+                rs.each do
+                    line = "#{rs.read(String)},#{rs.read(String)},#{ rs.read(String) }"
+                    # puts line 
+                    file.puts line
+                    # file.puts "#{rs.read(String)},#{rs.read(String)},#{ rs.read(String) }"
+                end
+            end
+        rescue  e 
+            puts "Error: #{e.message}"
+        end
+        file.close
+        puts "Data Exported"
+    when "export valid"
+        puts "Exporting VALID to exported_spdb_valid.csv ... "
+        File.delete("./exported_spdb_valid.csv") if File.exists? "./exported_spdb_valid.csv"
+        file = File.new("./exported_spdb_valid.csv", "w")
+        file.puts "Username,Password,Time"
+        begin 
+            db.query "select username.username, password.password , valid_passwords.date_time
+            from username, password, valid_passwords
+            where 
+            username.usernameid = valid_passwords.usernameid
+            and 
+            password.passwordid = valid_passwords.passwordid;" do |rs|
+                rs.each do
+                    line = "#{rs.read(String)},#{rs.read(String)},#{ rs.read(String) }"
+                    # puts line 
+                    file.puts line
+                    # file.puts "#{rs.read(String)},#{rs.read(String)},#{ rs.read(String) }"
+                end
+            end
+        rescue  e 
+            puts "Error: #{e.message}"
+        end
+        file.close
+        puts "Data Exported"
+    when "export passwords"
+        puts "Exporting VALID to exported_spdb_passwords.csv ... "
+        File.delete("./exported_spdb_passwords.csv") if File.exists? "./exported_spdb_passwords.csv"
+        file = File.new("./exported_spdb_passwords.csv", "w")
+        file.puts "ID,Password"
+        begin 
+            db.query "select * from password;" do |rs|
+                rs.each do
+                    line =  "#{rs.read(Int32)}, #{rs.read(String)}"
+                    file.puts line
+                end
+            end
+        rescue  e 
+            puts "Error: #{e.message}"
+        end
+        file.close
+        puts "Data Exported"
+    when "export usernames"
+        puts "Exporting VALID to exported_spdb_usernames.csv ... "
+        File.delete("./exported_spdb_usernames.csv") if File.exists? "./exported_spdb_usernames.csv"
+        file = File.new("./exported_spdb_usernames.csv", "w")
+        file.puts "ID,Username"
+        begin
+            db.query "select * from username;" do |rs|
+                rs.each do
+                    line =  "#{rs.read(Int32)}, #{rs.read(String)}"
+                    file.puts line
+                end
+            end
+        rescue  e
+            puts "Error: #{e.message}"
+        end
+        file.close
+        puts "Data Exported"
+
+
+    when "export"
+        puts "export [table]"
+        puts "ex. export passwords"
+
     when "help"
-        puts "help  usernames  passwords  valid  sprayed"
+        puts "help  usernames  passwords  valid  sprayed  export"
     end
 
 
